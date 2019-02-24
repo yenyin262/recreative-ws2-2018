cda233714bebb31317c31a7839982a01220ccf0d
Yen Yin Hing <yenyinf287@gmail.com>
2019-02-23 16:49:19 -0800

    front-page sass edit for mobile version

--- themes/recreative_apparel/front-page.php
+++ themes/recreative_apparel/front-page.php
@@ -8,6 +8,7 @@
 			<?php if (is_front_page() ) : ?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                 
 				</header>
 			<?php endif; ?>
 
