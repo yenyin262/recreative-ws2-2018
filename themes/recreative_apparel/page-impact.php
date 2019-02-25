d73b6f2f54b9a26d0a5544c160783a84fba8cbcd
risakanagawa <gw.09ltry@gmail.com>
2019-02-25 13:02:43 -0800

    add media queries in mixin

--- themes/recreative_apparel/page-impact.php
+++ themes/recreative_apparel/page-impact.php
@@ -2,22 +2,24 @@
 /*
 Template Name: impact
 */ 
+get_header();
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-    <div class="container">
+    <div class="impact-container">
     <?php
       $impacts = CFS()->get( 'impact_content' );
       foreach ( $impacts as $impact ) {?>
-          <div class="grid">
-          <div class="blue grid-impact-number"><?php echo $impact['impact_number'];?></div>
-          <div class="blue grid-impact-heading"><?php echo $impact['impact_heading'];?></div>
+          <section class="grid-impact">
+          <div class="blue-text grid-impact-number"><?php echo $impact['impact_number'];?></div>
+          <div class="blue-text grid-impact-heading"><?php echo $impact['impact_heading'];?></div>
           <div class="grid-impact-img"><img src='<?php echo $impact['impact_image'];?>' alt='Impact image'></div>
           <div class="grid-impact-text"><?php echo $impact['impact_blurb'];?></div>
-          </div>
+          </section>
       <?php }?>
       </div>
+
 		</main><!-- #main -->
 	</div><!-- #primary  -->
 
