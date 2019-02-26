7e984a035b44fbdeb5328ba197b6f2d8bd041199
Yen Yin Hing <yenyinf287@gmail.com>
2019-02-25 20:57:38 -0800

    fix nav bar and insert logo on front-page

--- themes/recreative_apparel/front-page.php
+++ themes/recreative_apparel/front-page.php
@@ -1,4 +1,11 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
 
+get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -30,13 +37,6 @@
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-/**
- * The main template file.
- *
- * @package RED_Starter_Theme
- */
 
-get_header(); ?>
 
 <?php get_footer(); ?>
