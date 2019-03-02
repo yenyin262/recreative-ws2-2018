6536ca94a8efec5b6566c383639d19276a2e9297
risakanagawa <gw.09ltry@gmail.com>
2019-03-02 15:39:31 -0800

    fix footer styling

--- themes/recreative_apparel/js/footer.js
+++ themes/recreative_apparel/js/footer.js
@@ -1,16 +1,34 @@
 (function($) {
-    'use strict';
-    console.log('footer')
-    // let checkBox = false;
-    $('.subscribe-email').on('focus', ()=> {
-        $('.subscribe-email').attr('placeholder', 'email');
-        $('.notification-check').css("display","block");
-    })
-    // $('.subscribe-form').on('mouseleave', function() {
-    //     $('.subscribe-email').attr('placeholder', 'Subscribe');
-    //     $('.subscribe-checkbox').css("display","none");
-    //     $('.subscribe-text').css("display","none");
-    // });
+  'use strict';
+  const emailBox = $('.subscribe-email');
+  const confirmText = $('.confirm-text');
+  const checkBox = $('.notification-check');
 
-  })(jQuery);
-  
+  emailBox.on('click', () => {
+    emailBox.attr('placeholder', 'email');
+    confirmText.css('display', 'block');
+    checkBox.toggle();
+    if ($(window).width() <= 500) {
+      $('.subscribe-form').css('display', 'block');
+      $('.social-icon-section').hide();
+    }
+  });
+  $('html').on('click', event => {
+    if (
+      !$(event.target).hasClass('subscribe-form') &&
+      !$(event.target).hasClass('subscribe-email') &&
+      !$(event.target).hasClass('subscribe-checkbox') &&
+      !$(event.target).hasClass('confirm-text')
+    ) {
+      emailBox.attr('placeholder', 'Subscribe');
+      checkBox.hide();
+      confirmText.css('display', 'none');
+      $('.social-icon-section').show();
+
+    }
+  });
+
+  //   $('.subscribe-form').on('focusout', function() {
+
+  //   });
+})(jQuery);
