c41ff04c8b655227be09b3f5e986c9dcab2cc189
Nosa Paath <adepaath@gmail.com>
2019-03-07 20:15:38 -0800

    carousel bug fixed

--- themes/recreative_apparel/js/general.js
+++ themes/recreative_apparel/js/general.js
@@ -2,7 +2,7 @@
   'use strict';
   const headerAnimation = () => {
     const speed = 1000;
-    var position = $('#primary').offset().top - 100;
+    var position = $('#primary').offset().top - 500;
     $("html, body").animate({scrollTop: position}, speed, "swing");
     return false;
   };
