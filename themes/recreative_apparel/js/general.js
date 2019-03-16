48714f06cb06ae871c5fea2a3a32e1b773e426cc
risakanagawa <gw.09ltry@gmail.com>
2019-03-16 16:32:17 -0700

    add scroll animation

--- themes/recreative_apparel/js/general.js
+++ themes/recreative_apparel/js/general.js
@@ -1,9 +1,33 @@
 (function($) {
   $(window).bind('load', function() {
     $('#menu-primary-menu li a[href]').each(function() {
-      if (this.href == window.location.href) 
+      if (this.href == window.location.href) {
+        $(this)
+          .parent()
+          .css('border-top', '5px solid #0545b2');
+      }
+    });
+
+    let scrollFlag = false;
+    $('html, body').animate(
       {
-          $(this).parent().css('border-top', '5px solid #0545b2');
+        scrollTop: $('.sticky-nav').offset().top - 500
+      },
+      800,
+      function() {
+        scrollFlag = true;
+      }
+    );
+
+    $(window).on('scroll', function() {
+      if (scrollFlag) {
+        $('html, body').animate(
+          {
+            scrollTop: $('.sticky-nav').offset().top
+          },
+          1000
+        );
+        $(window).off('scroll');
       }
     });
   });
