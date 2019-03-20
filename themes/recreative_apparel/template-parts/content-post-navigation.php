7a56e2b003af39b03da0bec2e909d642ce3095c8
Nosa Paath <adepaath@gmail.com>
2019-03-19 19:04:25 -0700

    header size

--- themes/recreative_apparel/template-parts/content-post-navigation.php
+++ themes/recreative_apparel/template-parts/content-post-navigation.php
@@ -6,7 +6,7 @@
         $nextThumbnailUrl = wp_get_attachment_image_src( get_post_thumbnail_id( $nextPost->ID ), $size = 'large', $icon = false );  
       ?>
       <div class="single-footer-image"style="background-image: url('<?php echo $nextThumbnailUrl[0]; ?>')">
-		  </div>
+      </div>
       <a href="<?php echo get_permalink( $nextPost->ID ); ?>">
         <div class="post-nav-title"><?php echo apply_filters( 'the_title', $nextPost->post_title ); ?></div>
       </a> 
