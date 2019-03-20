ec9eb63eee4fb7d4e75b606b8fa7f696c637e1a8
Nosa Paath <adepaath@gmail.com>
2019-03-19 20:17:03 -0700

    prev and next

--- themes/recreative_apparel/template-parts/content-post-navigation.php
+++ themes/recreative_apparel/template-parts/content-post-navigation.php
@@ -1,18 +1,15 @@
-  <div class="post-up-next"> 
-    
-    <?php
-      if(!empty($nextPost)){
-        $nextPost = get_next_post();
-        $nextThumbnailUrl = wp_get_attachment_image_src( get_post_thumbnail_id( $nextPost->ID ), $size = 'large', $icon = false );  
-      ?>
-      <div class="single-footer-image"style="background-image: url('<?php echo $nextThumbnailUrl[0]; ?>')">
-      </div>
-      <a href="<?php echo get_permalink( $nextPost->ID ); ?>">
-        <div class="post-nav-title"><?php echo apply_filters( 'the_title', $nextPost->post_title ); ?></div>
-      </a> 
-      
+<div class="post-up-next"> 
+  <?php
+  $nextPost = get_previous_post();
+    if(!empty($nextPost)){
+      $nextThumbnailUrl = wp_get_attachment_image_src( get_post_thumbnail_id( $nextPost->ID ), $size = 'large', $icon = false );  
+    ?>
+    <div class="single-footer-image"style="background-image: url('<?php echo $nextThumbnailUrl[0]; ?>')">
+    </div>
+    <a href="<?php echo get_permalink( $nextPost->ID ); ?>">
+      <div class="post-nav-title"><?php echo apply_filters( 'the_title', $nextPost->post_title ); ?></div>
+    </a> 
     <?php 
-    wp_reset_postdata();
-    } ?> 
-
+    wp_reset_postdata();  
+    }?>
 </div>
