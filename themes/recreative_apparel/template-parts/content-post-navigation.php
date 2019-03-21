11829a3cec8d0636252d980250285101ee83642a
Nosa Paath <adepaath@gmail.com>
2019-03-20 17:03:59 -0700

    post-navigation styling added

--- themes/recreative_apparel/template-parts/content-post-navigation.php
+++ themes/recreative_apparel/template-parts/content-post-navigation.php
@@ -6,8 +6,10 @@
     ?>
     <div class="single-footer-image"style="background-image: url('<?php echo $nextThumbnailUrl[0]; ?>')">
     </div>
-    <a href="<?php echo get_permalink( $nextPost->ID ); ?>">
+    <a class="nav-meta-box" href="<?php echo get_permalink( $nextPost->ID ); ?>">
+      <h1>UP NEXT</h1>
       <div class="post-nav-title"><?php echo apply_filters( 'the_title', $nextPost->post_title ); ?></div>
+      <div class="nav-author"><?php echo get_the_author(); ?></div>
     </a> 
     <?php 
     wp_reset_postdata();  
