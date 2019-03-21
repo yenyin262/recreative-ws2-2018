7f4b6255485d87483fb121f6e03254c80504eb12
risakanagawa <gw.09ltry@gmail.com>
2019-03-20 19:09:48 -0700

    remove animation

--- themes/recreative_apparel/js/general.js
+++ themes/recreative_apparel/js/general.js
@@ -1,5 +1,9 @@
 (function($) {
   $(window).bind('load', function() {
+    const navTop = $('.sticky-nav').offset().top;
+    const $window = $(window);
+
+    //Add current page to border top
     $('#menu-primary-menu li a[href]').each(function() {
       if (this.href == window.location.href) {
         $(this)
@@ -8,28 +12,26 @@
       }
     });
 
-    let scrollFlag = false;
-
-    $('html, body').animate(
-      { scrollTop: $('.sticky-nav').offset().top - 600}, 800,
-      function() {
-        scrollFlag = true;
-      }
-    );
+    $('html, body').animate({ scrollTop: navTop - 600 }, 800);
 
     $(window).on('scroll', function() {
-      if (scrollFlag) {
+      if ($window.hasClass('ra-blue-logo')) {
+        console.log('has blue');
+      }
+      if ($window.scrollTop() >= navTop) {
+        console.log('text-logo!');
         $('.ra-blue-logo').css('display', 'none');
-        $('html, body').animate(
-          {scrollTop: $('.sticky-nav').offset().top}, 1000, function(){
-            $('.rc-logo').css('display', 'none');
-            if(!($('body').hasClass('single-post'))){
-              $('.ra-front-page-logo').css('display', 'none');
-              $('.text-logo').css('display', 'block'); 
-            } 
-          }
-        );
-        $(window).off('scroll');
+        $('.rc-logo').css('display', 'none');
+        if (!$('body').hasClass('single-post')) {
+          $('.ra-front-page-logo').css('display', 'none');
+          $('.text-logo').css('display', 'block');
+        }
+      } else {
+        console.log('round logo');
+        $('.ra-blue-logo').css('display', 'block');
+        $('.white-logo').css('display', 'block');
+        $('.rc-logo').css('display', 'block');
+        $('.text-logo').css('display', 'none');
       }
     });
   });
